32c6409181d69bab98bc447fd5c289fd50b13b81
Brian Wandell <wandell@stanford.edu>
2013-10-01 10:12:55 -0700

    Start of a validation script.  More to do.

---
+++ Manuscript-PDGain/figures_scripts/v_MRQ.m
@@ -0,0 +1,28 @@
+%% Validate all of the Figure scripts
+%
+%
+%
+% AM VISTASOFT Team, 2013
+
+%% Set up the order and run
+
+Fig_brainRepeatability                        
+Fig_phantom                            
+Fig_Bilinear_Fit_PD_fWith_MultiCoilM0_andNoise
+Fig_MultiTissueT1reg    
+Fig_PD_G_and_M0_examples
+Fig_PD_estimate_T1reg                          
+Fig_PDxGainEqM0                             
+Fig_R1WFlinearRelations      
+
+
+CalRegsErrors                                   
+SimAllmodelFit                                 
+
+%% 
+SupFig_PD_estimate_All_models.m                   
+SupFig_PD_estimate_CorrReg.m                      
+SupFig_PD_estimate_T1reg_Intializatiions.m        
+SupFig_PD_estimate_ridgeReg.m                     
+
+%% END

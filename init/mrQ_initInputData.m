07aa61e78fd269324c89a13754f0be1da7e92b3b
Michael Perry <lmperry@stanford.edu>
2014-03-31 15:05:26 -0700

    ENH: Many changes to algorithms that choose nifti files for analysis

--- init/mrQ_initInputData.m
+++ init/mrQ_initInputData.m
@@ -50,6 +50,7 @@ function mrQ = mrQ_initInputData(mrQ)
 
 %% Process inputs and checks
 % mrQ.RawDir = '/biac4/wandell/data/westonhavens/upload/testlab/20130509_1152_4534';
+fprintf('[%s]\n',mfilename);
 
 if ~isfield(mrQ,'RawDir')
     error('No rawDir in mrQ structure');
@@ -58,7 +59,6 @@ end
 if ~isfield(mrQ,'fieldstrength')
     mrQ = mrQ_Set(mrQ,'fieldstrength',3);
 end
-
 
 
 %% Build a struct with the path to each nifti file in mrQ.RawDir
@@ -88,8 +88,8 @@ end
 nifti = {};
 
 % Read the parameters for each of the nifti files
+disp('Reading parameters from nifti files');
 for jj = 1:numel(niFiles)
-%     fprintf('Reading  %s\n',niFiles{jj});
     nifti{end+1} = niftiGetParamsFromDescrip(niFiles{jj});
 end
 
@@ -98,46 +98,87 @@ nifti(cellfun(@isempty,nifti)) = [];
 
 
 %% Flip Angle
-% The flip angles we want
-reqfa = [10, 20, 30, 4];
 
-% This specifies how many degrees, +/- the actual flip angle must be within compared to the 
-% prescribed flip angles above. 
-fad  = 2;
+% Rules: 
+%	- Data must be 4D of same size (multi-coil) - don't know about the same size thing? 
+%	- Minimum of 2 unique flip angles with 1 <=10 and 1 >= 10. 
+%	- FA must be <=30
+%	- TE <= 3
+% 	- TI == 0 
+%	- OFF RESONANCE == 0
 
-% Get the index for files with flip angles we want
+disp('Looking for nifti files with valid flip angles...');
 fa = {};
 for ii = 1:numel(nifti)
-    if isfield(nifti{ii},'fa') && ( isfield(nifti{ii},'rs') || isfield(nifti{ii},'r') )
-    	for xx = 1:numel(reqfa)
-    		if ( nifti{ii}.fa >=  (reqfa(xx) - fad) ) && ( nifti{ii}.fa <=  (reqfa(xx) + fad) )
-    			fa{end+1} = ii;
-    		end
-    	end
-    end
+	if isfield(nifti{ii},'fa') && ( isfield(nifti{ii},'rs') || isfield(nifti{ii},'r') )
+		if ( nifti{ii}.fa <= 30 ) && ( nifti{ii}.te <= 3 ) && ( nifti{ii}.ti == 0 )
+			fa{end+1} = ii;
+		end
+	end
 end
 
+fprintf('%d nifti files found:\n',numel(fa));
+for nn = 1:numel(fa)
+	[p, f, e ] = fileparts(nifti{fa{nn}}.niftiFile); 
+	thename = [f, e];
+	epoch = explode('/',p); 
+	fprintf('\t%s/%s: \tFlip angle = %d\n',epoch{end},[f,e],nifti{fa{nn}}.fa);
+end
+
+% Is 1 flip angle less than 10 and one greater than 10
+f = zeros(size(fa));
+for fas = 1:numel(fa)
+	f(fas) = nifti{fa{fas}}.fa;
+end
+if numel(f) < 3 && ( isempty(find(f > 10)) || isempty(find(f < 10)) ) 
+	warning('There are fewer than 3 valid nifti files and those flip angles are not consistent with this analysis!');
+end
+			
 
 %% Inversion Time
-reqit = [2400, 1200, 400, 50];
+% RULES: 
+%	- 3 unique TIs 
+%	- TI must be non-zero
+% 	- TI must be <=2500 * 
+%	- Constant TE
 
-% Difference in ms allowed between the prescribed IT and actual IT in the dicom. 
-itd = 20;
-
-% Get the IT for the SEIR structure
+disp('Looking for nifti files with valid inversion times...');
 it ={};
 for jj = 1:numel(nifti)
     if isfield(nifti{jj},'ti')
-		for itx = 1:numel(reqit)    
-	        if ( nifti{jj}.ti >= reqit(itx) - itd ) && ( nifti{jj}.ti <= reqit(itx) + itd )
-    	        it{end+1} = jj;
-        	end
+    	if ( nifti{jj}.ti ~=0 ) && (nifti{jj}.ti <= 2500)
+    		it{end+1} = jj;
         end
     end
 end
 
+fprintf('%d nifti files found:\n',numel(it));
+for nn = 1:numel(it)
+	[p, f, e ] = fileparts(nifti{it{nn}}.niftiFile); 
+	epoch = explode('/',p); 
+	fprintf('\t%s/%s: \tInversion time = %d\n',epoch{end},[f,e],nifti{it{nn}}.ti);
+end
 
-%% Construct inputData structures
+% Find out if the inversion times are unique
+t = zeros(size(it));
+for nit = 1:numel(it)
+	t(nit) = nifti{it{nit}}.ti;
+end
+if numel(unique(t)) ~= numel(it)
+	warning('INVERSION TIMES ARE NOT UNIQUE!');
+end
+
+% Find out if the TEs are constant
+e = zeros(size(it));
+for ne = 1:numel(it)
+	e(ne) = nifti{it{ne}}.te;
+end
+if numel(unique(e)) ~= 1
+	warning('ECHO TIMES ARE NOT CONTSTANT!');
+end
+
+
+%% Construct inputData structure
 inputData_spgr = {};
 
 % Loop over the fa and it structs and populate the input data fields
@@ -166,7 +207,7 @@ inputData_spgr.rawDir = mrQ.RawDir;
 mrQ = mrQ_Set(mrQ,'inputdata_spgr',inputData_spgr);
 mrQ = mrQ_Set(mrQ,'inputdata_seir',inputData_seir);
 
-if numel(it) <2 || numel(fa) <2
+if numel(it) < 3 || numel(fa) < 2
     fprintf('[%s] - Files required for mrQ not found in %s\n',mfilename,mrQ.RawDir);
     else
     fprintf('[%s] - Files required for mrQ found!\n',mfilename);

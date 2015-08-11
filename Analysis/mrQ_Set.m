b4df81243284b1782dcf5e6945feaf59b764792c
jonathanbain <jonathan.bain@mail.huji.ac.il>
2015-08-11 14:20:28 +0300

    Added some commenting to mrQ_Set

--- Analysis/mrQ_Set.m
+++ Analysis/mrQ_Set.m
@@ -26,41 +26,79 @@ function mrQ = mrQ_Set(mrQ,param,varargin,saveflag)
 %    Parameter list and associated arguments:
 %
 %     ~PARAM~              ~VARARGIN~ 
-%  1) alignflag
-%  2) arrangerawflag
-%  3) autoacpc
+%  1) alignflag               - Enter to 1 if you want to align the SEIR 
+%                                   slices, 0 (default) if not. If the
+%                                   SEIR data has more than a few slices,
+%                                   it is a good idea to try to align them.
+%  2) arrangerawflag          - [no longer in use]
+%  3) autoacpc                - Enter 1 (default) if you want mrQ to 
+%                                   perform the AC-PC alignment 
+%                                   automatically, or enter 0 if you want 
+%                                   to perform it manually (using 
+%                                   mrAnatAverageAcpcNifti.m).
 %  4) brakeaftert1            - [no longer in use]
 %  5) brakeaftervisualization - [no longer in use]
-%  6) channels
-%  7) check                   - enter 1, if an interactive image is used to detect movement
-%  8) clobber                 - enter "true" or "false" to redo the analysis
+%  6) channels                - [no longer in use]
+%  7) check                   - Enter 1 if an interactive image is used to
+%                                    detect movement, or 0 if not (default)
+%  8) clobber                 - Enter 1 to overwrite existing data and
+%                                    reprocess, or 0 (default) not to 
+%                                    reprocess. As a mrQ_Set input it
+%                                    determines whether to redo the whole
+%                                    mrQ, but clobber also appears in
+%                                    functions within mrQ, to detect
+%                                    whether to reprocess a particular
+%                                    function or sub-function.
 %  9) coilweights
 % 10) complexflag
-% 11) fieldstrength
-% 12) freesurfer
-% 13) inputdata_seir
-% 14) inputdata_spgr
-% 15) interp
-% 16) lsq
-% 17) lw
-% 18) mmpervox
+% 11) fieldstrength          - Enter the field strength of the scanner.
+% 12) freesurfer             - Enter 1 if FreeSurfer is available for use,
+%                                     or 0 if not (default).
+% 13) inputdata_seir         - Enter a string naming the directory of where 
+%                                    the SEIR data is located
+% 14) inputdata_spgr         - Enter a string naming the directory of where 
+%                                    the SPGR data is located
+% 15) interp                 - Enter 1 (default) to use the trilinear
+%                                    method of interpolation, or 7 to use
+%                                    the b-spline method. This is used in
+%                                    aligning the SPGR images.
+% 16) lsq                    - Enter 1 to run the least squares model for 
+%                                    the T1-M0 fit, or 0 not to (default).
+% 17) lw                     - Enter 1 (default) to run the linear weighted
+%                                    model for the T1-M0 fit, or 0 not to.
+% 18) mmpervox               - Enter a 3x1 vector of the image resolution.
+%                                    The default is the NIfTI's resolution.
 % 19) muti_coil_im
-% 20) pdfit_method           - for M0-to-PD fit. 1= T1 single coil (default); 2= multi-coils; 3= both
+% 20) pdfit_method           - Enter 1 for T1 single coil (default), 2 for 
+%                                    multi-coil, or 3 for both. This is 
+%                                    used in the M0-to-PD fit.
 % 21) permutation
-% 22) polydeg                - the polynomial degree to fit to the Gain (default is 3)
+% 22) polydeg                - Enter a scalar for the polynomial degree to 
+%                                     fit to the Gain (default is 3)
 % 23) proclus                - [no longer in use]
 % 24) reset_raw_dir
-% 25) rawdir                 - directory of raw image: a string 'path', as defined in mrQ_Create
-% 26) refim                  - a path to an image to use as a refernce: a string 'path'
+% 25) rawdir                 - Enter a string naming the directory of the 
+%                                     raw image, as defined in mrQ_Create
+% 26) refim                  - Enter the path to a NIfTI file to use as a
+%                                     reference image. If empty, the
+%                                     default is that the SPGR with a
+%                                     similar contrast to the T1-weighted
+%                                     image will be selected, and the user 
+%                                     will be asked to mark the ac-pc using 
+%                                     mrAnatAverageAcpcNifti.m.
 % 27) runfreesurfer
-% 28) seir                   - list of SEIR scan numbers: a cell of strings, e.g. {'001' 002'}
+% 28) seir                   - Enter a cell of strings (e.g. {'001' 002'}) 
+%                                     as the list of SEIR scan numbers. 
 % 29) siemens                - [no longer in use]
 % 30) siemensdicom           - [no longer in use]
-% 31) skip
-% 32) spgr                   - list of SPGR scan numbers: a cell of strings, e.g. {'001' 002'}
-% 33) sub                    - the name of subject: a string 'name'
-% 34) sungrid                - enter 1, to use the SunGrid calls
-% 35) useabs
+% 31) skip                   - Enter a 1xN vector of scans to skip. Default
+%                                   is not to skip any.
+% 32) spgr                   - Enter a cell of strings (e.g. {'003' 004'}) 
+%                                     as the list of SPGR scan numbers.  
+% 33) sub                    - Enter a string as the name of subject.
+% 34) sungrid                - Enter 1 to use the SunGrid calls, or 0 not
+%                                     to (default).
+% 35) useabs                 - [no longer in use]
 %
 % The following are checkers to determine whether or not a step has been
 % performed. Inputting 1 means the step has been done, so don't redo it,
